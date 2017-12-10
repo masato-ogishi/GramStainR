96bb169f4606b15fcb1f9428ad8a9c2948c03845
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2017-12-10 18:00:21 +0900

    Add files via upload

--- R/bacteriaClassification.R
+++ R/bacteriaClassification.R
@@ -3,7 +3,7 @@
 #' @param trainDF A training dataframe.
 #' @param testDF A testing dataframe.
 #' @param validDF (Optional) A dataframe for external validation.
-#' @param withReference Whether the \code{bacteriaDetectionResult} contains bacteria labels for referencing purpose. If set TRUE, letters from original image file names are used as bacteria labels.
+#' @param withReference Whether the \code{bacteriaDetectionResult} contains bacteria labels for referencing purpose. If set TRUE, letters from original image file names are used as bacteria labels. Currently, only a binary classification is implemented.
 #' @param predictType At which level the predictive performance is evaluated. Can be either "Bacteria" or "Image".
 #' @param seed A random seed.
 #' @param max_mem_size A memory limit for H2O Java machine learning engine.
@@ -12,6 +12,8 @@
 #' @importFrom dplyr mutate
 #' @importFrom dplyr group_by
 #' @importFrom dplyr summarise
+#' @importFrom dplyr ungroup
+#' @importFrom tidyr gather
 #' @importFrom readr write_csv
 #' @importFrom stringr str_detect
 #' @importFrom caret confusionMatrix 
@@ -38,23 +40,27 @@ bacteriaClassification <- function(trainDF, testDF, validDF=NULL,
   df_lb <- as.data.frame(aml@leaderboard)
   readr::write_csv(df_lb, "./Results/LeaderBoard.csv")
   View(df_lb)
-  best_model <- h2o::h2o.getModel(dplyr::filter(df_lb, !stringr::str_detect(model_id, "StackedEnsemble"))[["model_id"]][[1]])
+  best_model <- h2o::h2o.getModel(df_lb[["model_id"]][[1]])
   
   ## Evaluate the best classifier
   predEval <- function(evalH2ODF, H2OMod, withReference=T, predictType=c("Bacteria","Image"), outputHeader=""){
     dir.create(outputHeader, showWarnings=F, recursive=T)
     evalDF <- as.data.frame(evalH2ODF)
     predDF <- as.data.frame(predict(H2OMod, evalH2ODF))
-    predDF <- data.frame("PredictedBacteria"=predDF[[1]], "Probability"=predDF[[2]])  ## binary classification only
+    colnames(predDF)[1] <- "PredictedBacteria"
     lev <- levels(predDF$"PredictedBacteria")
     if(withReference==F){
       predDF <- data.frame("Source"=evalDF$"Source", predDF)
       if(predictType=="Bacteria"){
         return(predDF)
       }else if(predictType=="Image"){
-        predDF <- predDF %>% 
-          dplyr::group_by(Source) %>% 
-          dplyr::summarise(PredictedBacteria=which.max(table(PredictedBacteria)), Probability=mean(Probability)) %>%
+        predDF <- predDF %>%
+          tidyr::gather(Prob.PredBacteria, Probability, -Source, -PredictedBacteria) %>%
+          dplyr::group_by(Source, Prob.PredBacteria) %>%
+          dplyr::summarise(Probability=mean(Probability)) %>%
+          dplyr::ungroup() %>%
+          dplyr::group_by(Source) %>%
+          dplyr::summarise(PredictedBacteria=lev[which.max(Probability)], Probability=Probability[1]) %>%
           dplyr::mutate(PredictedBacteria=factor(PredictedBacteria, levels=lev))
         return(predDF)
       }
@@ -64,12 +70,16 @@ bacteriaClassification <- function(trainDF, testDF, validDF=NULL,
         sink(paste0(outputHeader, "/ConfusionMatrix.txt"))
         print(caret::confusionMatrix(predDF$"PredictedBacteria", predDF$"Bacteria"))
         sink()
-        classifierplots::classifierplots_folder(test.y=2-as.numeric(predDF$"Bacteria"), pred.prob=predDF$"Probability", folder=outputHeader)
+        classifierplots::classifierplots_folder(test.y=2-as.numeric(predDF$"Bacteria"), pred.prob=predDF[[lev[1]]], folder=outputHeader)
         return(predDF)
       }else if(predictType=="Image"){
-        predDF <- predDF %>% 
-          dplyr::group_by(Source) %>% 
-          dplyr::summarise(Bacteria=unique(Bacteria), PredictedBacteria=which.max(table(PredictedBacteria)), Probability=mean(Probability)) %>%
+        predDF <- predDF %>%
+          tidyr::gather(Prob.PredBacteria, Probability, -Bacteria, -Source, -PredictedBacteria) %>%
+          dplyr::group_by(Bacteria, Source, Prob.PredBacteria) %>%
+          dplyr::summarise(Probability=mean(Probability)) %>%
+          dplyr::ungroup() %>%
+          dplyr::group_by(Source) %>%
+          dplyr::summarise(PredictedBacteria=lev[which.max(Probability)], Probability=Probability[1]) %>%
           dplyr::mutate(PredictedBacteria=factor(PredictedBacteria, levels=lev))
         sink(paste0(outputHeader, "/ConfusionMatrix.txt"))
         print(caret::confusionMatrix(predDF$"PredictedBacteria", predDF$"Bacteria"))
@@ -82,9 +92,11 @@ bacteriaClassification <- function(trainDF, testDF, validDF=NULL,
   
   predDFList <- list()
   if(is.null(validDF)){
+    predDFList[["Train"]] <- predEval(df_train, best_model, withReference=withReference, predictType=predictType, outputHeader="./Results/Training/")
     predDFList[["Test"]] <- predEval(df_test, best_model, withReference=withReference, predictType=predictType, outputHeader="./Results/HoldOutValidation/")
   }else{
     df_valid <- h2o::as.h2o(validDF)
+    predDFList[["Train"]] <- predEval(df_train, best_model, withReference=withReference, predictType=predictType, outputHeader="./Results/Training/")
     predDFList[["Test"]] <- predEval(df_test, best_model, withReference=withReference, predictType=predictType, outputHeader="./Results/HoldOutValidation/")
     predDFList[["Valid"]] <- predEval(df_valid, best_model, withReference=withReference, predictType=predictType, outputHeader="./Results/ExternalValidation/")
   }
