effa32fe8e62a52e3b2bee72f9556f2f653396c7
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2017-12-15 17:51:36 +0900

    Update bacteriaDetection.R

--- R/bacteriaDetection.R
+++ R/bacteriaDetection.R
@@ -2,7 +2,6 @@
 #' 
 #' @param originalImageFileName The input image file name. Should be provided as a combination of letters and numbers, since letters are interpreted as image source labels and numbers as image IDs. 
 #' @param originalImageFileNameList A list of image file names.
-#' @param windowSize An integer representing the window pixel size for capturing a single bacteria. User must specify this by manually examining image data provided.
 #' @importFrom dplyr %>%
 #' @importFrom dplyr bind_cols
 #' @importFrom dplyr mutate
@@ -41,7 +40,23 @@
 #' @export
 #' @rdname bacteriaDetection
 #' @name bacteriaDetection
-bacteriaDetection_Single <- function(originalImageFileName, windowSize=150){
+bacteriaDetection_Single <- function(originalImageFileName){
+  # Parameters
+  windowSize=100
+  ws=round(windowSize/2)
+  brushSize=5
+  thresholdAdjustRatio=1.2
+  minAreaSize=300
+  maxAreaSize=1000
+  minOutlierFeature=0.1
+  maxOutlierFeature=0.5
+  #robustRange <- function(x, trim=0.2, n.sd=2){
+  #  x.log.dist <- psych::describe(log10(x), trim=0.2)
+  #  x.log.range <- x.log.dist$trimmed + x.log.dist$mad/0.675 * n.sd * c(-1, 1)  ## mean +/- 1sd => 67%, 2sd -> 95%
+  #  x.range <- 10^x.log.range
+  #  return(x.range)
+  #}
+  
   # Load the image data [RGB]
   ext <- data.table::last(unlist(stringr::str_split(basename(originalImageFileName), stringr::fixed("."))))
   header <- stringr::str_replace_all(originalImageFileName, paste0(".", ext, "$"), "")
@@ -51,9 +66,9 @@ bacteriaDetection_Single <- function(originalImageFileName, windowSize=150){
   # Thresholding
   img.thr <- EBImage::Image(EBImage::combine(lapply(EBImage::getFrames(img.Image), function(fr){
     img.neg <- 1-fr
-    thr <- EBImage::otsu(img.neg)*1.2
+    thr <- EBImage::otsu(img.neg)*thresholdAdjustRatio
     img.thr <- img.neg > thr
-    img.thr <- img.thr %>% EBImage::dilate(EBImage::makeBrush(5, shape="disc")) %>% EBImage::fillHull()
+    img.thr <- img.thr %>% EBImage::dilate(EBImage::makeBrush(brushSize, shape="disc")) %>% EBImage::fillHull()
     img.thr <- 1- img.thr*img.neg
     return(img.thr)
   })), colormode='Color')
@@ -66,7 +81,7 @@ bacteriaDetection_Single <- function(originalImageFileName, windowSize=150){
   
   # Create an object mask based on edges
   msk <- imager::as.cimg(edg) %>% imager::threshold() %>% imager::clean(1) %>% 
-    EBImage::as.Image() %>% EBImage::dilate(EBImage::makeBrush(5, shape="disc")) %>% EBImage::fillHull() %>% 
+    EBImage::as.Image() %>% EBImage::dilate(EBImage::makeBrush(brushSize, shape="disc")) %>% EBImage::fillHull() %>% 
     EBImage::rotate(90) %>% EBImage::flop()
   if(length(dim(msk))>=4){
     dm <- 1:length(dim(msk))
@@ -92,24 +107,12 @@ bacteriaDetection_Single <- function(originalImageFileName, windowSize=150){
     as.data.frame(EBImage::computeFeatures.shape(img.bw))
   ) %>% 
     dplyr::mutate(ObjID=1:nrow(.)) %>%
-    dplyr::mutate(m.theta=m.theta*180/pi) %>%   ## convert radian to degree
-    dplyr::filter(m.majoraxis<=windowSize) %>%  ## remove too large debris...
-    dplyr::filter(s.area>=windowSize^2/30)      ## remove too small debris...
-  
+    dplyr::mutate(m.theta=m.theta*180/pi)   ## convert radian to degree
   featureDF[["OutlierFeature"]] <- (featureDF$s.perimeter/featureDF$s.area)*(featureDF$s.radius.max/featureDF$s.radius.min)
-  robustRange <- function(x, trim=0.2, n.sd=2){
-    x.log.dist <- psych::describe(log10(x), trim=0.2)
-    x.log.range <- x.log.dist$trimmed + x.log.dist$mad/0.675 * n.sd * c(-1, 1)  ## mean +/- 1sd => 67%, 2sd -> 95%
-    x.range <- 10^x.log.range
-    return(x.range)
-  }
-  outlier.range <- robustRange(featureDF$OutlierFeature, trim=0.2, n.sd=1)
-  featureDF <- featureDF %>% dplyr::filter(outlier.range[1]<=OutlierFeature & OutlierFeature<=outlier.range[2])
-  size.range <- robustRange(featureDF$s.area, trim=0.2, n.sd=1)
-  featureDF <- featureDF %>% dplyr::filter(size.range[1]<=s.area & s.area<=size.range[2])
-  
-  ws <- round(windowSize/2)
   featureDF <- featureDF %>%
+    dplyr::filter(m.majoraxis<=windowSize) %>%                      
+    dplyr::filter(minAreaSize<=s.area & s.area<=maxAreaSize) %>%   
+    dplyr::filter(minOutlierFeature<=OutlierFeature & OutlierFeature<=maxOutlierFeature) %>%
     dplyr::filter(1<=round(m.cx-ws) & round(m.cx+ws)<=dim(img.bw)[1] & 1<=round(m.cy-ws) & round(m.cy+ws)<=dim(img.bw)[2]) %>%
     dplyr::select(ObjID, setdiff(colnames(.), "ObjID"))
   if(nrow(featureDF)==0) return(NULL)
@@ -165,16 +168,12 @@ bacteriaDetection_Single <- function(originalImageFileName, windowSize=150){
 #' @export
 #' @rdname bacteriaDetection
 #' @name bacteriaDetection
-bacteriaDetection <- function(originalImageFileNameList, windowSize=150){
+bacteriaDetection <- function(originalImageFileNameList){
   imageSourceList <- lapply(
     originalImageFileNameList, 
     function(originalImageFileName){sub(pattern="(.*)\\..*$", replacement="\\1", basename(originalImageFileName))}
   )
-  res <- pbapply::pblapply(
-    originalImageFileNameList,
-    bacteriaDetection_Single, 
-    windowSize=windowSize
-  )
+  res <- pbapply::pblapply(originalImageFileNameList, bacteriaDetection_Single)
   names(res) <- imageSourceList
   return(res)
 }
