1bf929b29e813b0f98706c8db6582e1ccc5dae3b
Masato Ogishi <22980126+masato-ogishi@users.noreply.github.com>
2017-12-22 18:46:22 +0900

    Update bacteriaClassification.R

--- R/bacteriaClassification.R
+++ R/bacteriaClassification.R
@@ -30,6 +30,7 @@ bacteriaClassification <- function(trainDF, testDF, validDF=NULL,
                                    max_mem_size="6G"){
   set.seed(seed)
   dir.create(outputDir, showWarnings=F, recursive=T)
+  h2o::h2o.init(ip="localhost", port=seed, max_mem_size=max_mem_size, nthreads=6)
 
   ## Define bacteria species to be classified
   lev <- levels(trainDF$"Bacteria")
@@ -43,7 +44,6 @@ bacteriaClassification <- function(trainDF, testDF, validDF=NULL,
     dplyr::select(Bacteria, setdiff(colnames(.), "Bacteria"))
 
   ## Train bacteria classifiers
-  h2oServer <- h2o::h2o.init(ip="localhost", port=seed, max_mem_size=max_mem_size, nthreads=6)
   df_train <- h2o::as.h2o(trainDF)
   df_test <- h2o::as.h2o(testDF)
   aml <- h2o::h2o.automl(
@@ -60,7 +60,7 @@ bacteriaClassification <- function(trainDF, testDF, validDF=NULL,
   df_lb <- as.data.frame(aml@leaderboard)
   View(df_lb)
   best_model <- h2o::h2o.getModel(df_lb[["model_id"]][[1]])
-  try(h2o::h2o.saveModel(object=best_model, path=outputDir, force=T), silent=T)
+  h2o::h2o.saveModel(object=best_model, path=outputDir, force=T)
 
   ## Evaluate the best classifier
   predEval <- function(evalH2ODF, H2OMod){
